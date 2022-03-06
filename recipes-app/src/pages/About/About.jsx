e5c992f17a963fc28dcc0a994cd79912902b0f60
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-03-06 23:42:00 +0700

    about and home page

--- recipes-app/src/pages/About/About.jsx
+++ recipes-app/src/pages/About/About.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
+import './About.scss'
 
 export default function About() {
     return (
         <div>
+            {/* Header */}
             <Header />
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit illo architecto autem, iste minima eligendi porro optio ab nobis pariatur voluptas sequi consectetur maiores ipsa doloremque laudantium nemo id a! Nihil saepe officiis molestias labore expedita voluptatum dignissimos harum? Error quidem ratione sequi, necessitatibus doloribus id excepturi voluptates dignissimos praesentium quis sit velit possimus sunt quibusdam cupiditate, recusandae, aspernatur ullam porro repellat. Dolor, necessitatibus ullam. Ullam minima in ipsa quasi ipsum corrupti aperiam hic nulla? Libero quisquam exercitationem animi itaque enim nostrum, at dolorum laudantium beatae natus vitae excepturi. Temporibus sint nihil nostrum iusto fuga doloribus quo saepe, sit expedita provident porro impedit cum pariatur officiis perferendis fugiat explicabo facere. Suscipit fugiat facilis animi eum iusto ipsum perferendis veritatis labore rem. Numquam ducimus maiores atque et voluptatum minus unde debitis amet </div>
 
+            {/* About */}
+            <div className=".about">
+                <div className="about__image"></div>
+                <Container>
+                    <div className="about__section">
+                        <h2>ABOUT MAXIM LAVROV</h2>
+                        <hr></hr>
+                        <div className="about__content">
+                            <h3>THE EARLY DAYS</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore inventore laboriosam sequi minima sint veniam obcaecati necessitatibus. Quod accusamus a optio in, aliquid explicabo veniam perferendis expedita exercitationem sit fugit. Sed blanditiis doloribus, accusamus voluptatum minus inventore debitis vero reprehenderit et eveniet sint unde laudantium ad velit nesciunt neque repellendus earum nemo aperiam? Minima quia ipsum quae earum id accusamus possimus autem aliquid provident assumenda, maxime laborum rerum, veniam delectus libero fugiat magnam soluta! Exercitationem maiores aspernatur vitae tempora iste hic obcaecati laboriosam distinctio pariatur neque natus voluptas iusto quasi nemo earum, et amet nisi quibusdam voluptates vero autem atque?</p>
+                        </div>
+
+                        <div className="about__content">
+                            <h3>MICHELIN-STARRED RESTAURANTS</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur vitae, aperiam perferendis adipisci sapiente at a, quas ducimus officia dolores quis commodi eligendi quae deleniti quo molestias sunt dignissimos et libero distinctio saepe nam modi omnis. Quos assumenda nobis placeat tempora deleniti quo at inventore quae asperiores nostrum voluptas blanditiis, quia eius doloribus? Ipsa velit corrupti porro eveniet facilis, excepturi at tempore accusantium similique odio doloremque temporibus, maiores quo cumque, est ea. Enim deleniti rem repellendus neque. Ut doloribus repellendus sequi aperiam earum autem voluptate dolor fuga, vitae odit deserunt incidunt obcaecati architecto illum repudiandae modi id quasi quis facere expedita nobis. Labore quo eos quam natus, modi assumenda veritatis ipsa sint eaque, sapiente distinctio numquam! Enim accusamus quibusdam possimus? Quo sunt aut odio odit, cupiditate consequatur error quod corporis! Unde qui veritatis ea, sapiente accusantium praesentium dignissimos doloremque voluptates maxime ab ducimus libero quasi eius incidunt quisquam dolorum! Assumenda?</p>
+                        </div>
+
+                        <div className="about__content">
+                            <h3>GLOBAL RESTAURANTS</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae quas, fuga facilis quia sunt eligendi vero veniam vel excepturi ducimus sed aliquam a. Voluptates nostrum tempora quaerat odio ipsum adipisci, optio accusantium molestiae consequuntur dolorum mollitia, quidem, provident quia sit? Quaerat laboriosam voluptate non ullam pariatur rem eligendi vel tempora, inventore aliquam excepturi adipisci facere, mollitia praesentium hic maxime aliquid nulla quasi delectus? Velit, molestiae explicabo, sint, officia voluptates possimus adipisci autem impedit qui porro officiis ratione voluptas suscipit ex nostrum! Expedita dolorem repellendus repellat voluptatibus quibusdam deleniti numquam libero. Molestiae rerum qui sit distinctio, vel repellendus! Provident nihil esse cumque ut non impedit, suscipit ex consequatur magnam optio modi at sapiente porro quam, corporis aliquam perspiciatis ipsa error, illo facere. Tempore quia saepe eligendi nam repudiandae rem nulla quas in. Consequuntur suscipit perspiciatis doloremque est minus ea repudiandae omnis molestiae. Assumenda ad illum eveniet repudiandae optio quam non debitis quo, maiores repellendus sed consequatur neque! Autem voluptatibus aliquid beatae magni impedit, voluptatem itaque non, consequuntur molestias laborum ea! Unde odit assumenda, minus quia, animi atque cumque, illo vero ratione tempora impedit! Ipsa rerum, esse minus non a beatae cum, sunt recusandae ratione aspernatur magni accusamus! Consequatur similique nostrum odit ipsum nemo dolorum iure amet veritatis magni, ad itaque quas debitis. Voluptates quis dolorum eius, reiciendis eligendi deserunt suscipit eos omnis nisi minima, cupiditate doloribus eveniet unde maiores commodi magni velit deleniti ipsum alias eaque non totam. Dignissimos repudiandae, cupiditate minus eligendi esse sint illum. Nisi, dolor sed alias ipsum, non modi aliquid perferendis tenetur quaerat, excepturi ullam. Earum nobis eum provident nisi et, recusandae maxime nemo sequi quam aliquid dolor excepturi ex vitae laborum! Facere exercitationem, repellendus tempora omnis autem esse blanditiis saepe. Alias officiis, totam reprehenderit expedita fugit iusto quam quod, illum assumenda iure officia nostrum, dignissimos obcaecati.</p>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+
+            {/* Footer */}
             <Footer />
         </div>
     )
--- recipes-app/src/pages/Home/Home.jsx
+++ recipes-app/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header/Header'
-import { Carousel, Container } from "react-bootstrap"
+import { Button, Carousel, Col, Container, Row } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 import './Home.scss';
 import Footer from '../components/Footer/Footer';
@@ -50,12 +50,136 @@ export default function Home() {
         </Carousel.Item>
       </Carousel>
 
-      <div className="choice-section">
-        <Container>
-          <div className="section__title"></div>
-        </Container>
-      </div>
+      <div className="main">
+        {/* Editor Choice */}
+        <div className="choice">
+          <Container>
+            <div className="title choice__title">
+              <h3>EDITOR'S CHOICE</h3>
+            </div>
+            <Row>
+              <Col xs="3"><a href="#"><img src={require(`../../assets/images/pick1.jpg`)} alt="" /></a></Col>
+              <Col xs="3"><a href="#"><img src={require(`../../assets/images/pick2.jpg`)} alt="" /></a></Col>
+              <Col xs="3"><a href="#"><img src={require(`../../assets/images/pick3.jpg`)} alt="" /></a></Col>
+              <Col xs="3"><a href="#"><img src={require(`../../assets/images/pick4.jpg`)} alt="" /></a></Col>
+            </Row>
+          </Container>
+        </div>
 
+        {/* Features  */}
+        <div className="features">
+          <Container className="">
+            <div className="title features__title">
+              <h3>SPECIAL FEATURES</h3>
+            </div>
+            <Row>
+              <Col xs="4">
+                <div className="features__inner">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} fill="currentColor" className="bi bi-list-ul" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                    </svg>
+                  </span>
+                  <h4>COOKING METHOD</h4>
+                </div>
+              </Col>
+              <Col xs="4">
+                <div className="features__inner">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
+                      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                    </svg>
+                  </span>
+                  <h4>COOKING PERIOD</h4>
+                </div>
+              </Col>
+              <Col xs="4">
+                <div className="features__inner">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} fill="currentColor" className="bi bi-bookmark-star" viewBox="0 0 16 16">
+                      <path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z" />
+                      <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+                    </svg>
+                  </span>
+                  <h4>SAVE RECIPES</h4>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+
+        {/* Recipes */}
+        <div className="recipes">
+          <Container>
+            <div className="title recipes__title">
+              <h3>RECIPES</h3>
+            </div>
+            <Row>
+              <Col xs="3">
+                <div className="card" style={{ width: 100 + "%", height: 100 + "%" }}>
+                  <img src={require(`../../assets/images/recipes1.jpg`)} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <div className="recipes__time">
+                      <span>
+                        MEDIUM
+                      </span>
+                    </div>
+                    <h5 className="recipes__title">Indian-inspired frittata</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col xs="3">
+                <div className="card" style={{ width: 100 + "%", height: 100 + "%" }}>
+                  <img src={require(`../../assets/images/recipes2.jpg`)} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <div className="recipes__time">
+                      <span>
+                        HARD
+                      </span>
+                    </div>
+                    <h5 className="recipes__title">Joe Wick's Ravioli alla Napoletana</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col xs="3">
+                <div className="card" style={{ width: 100 + "%", height: 100 + "%" }}>
+                  <img src={require(`../../assets/images/recipes3.jpg`)} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <div className="recipes__time">
+                      <span>
+                        MEDIUM
+                      </span>
+                    </div>
+                    <h5 className="recipes__title">Malaysian-style whole fish</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col xs="3">
+                <div className="card" style={{ width: 100 + "%", height: 100 + "%" }}>
+                  <img src={require(`../../assets/images/recipes4.jpg`)} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <div className="recipes__time">
+                      <span>
+                        EASY
+                      </span>
+                    </div>
+                    <h5 className="recipes__title">Best roast leg of lamb</h5>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+            <div className="recipes__button">
+              <Button variant="outline-warning" href="/recipes">WATCH MORE</Button>
+            </div>
+          </Container>
+        </div>
+
+        {/* Feedback */}
+        <div className="feedback">
+          
+        </div>
+      </div>
       {/* Footer */}
       <Footer />
     </div>
